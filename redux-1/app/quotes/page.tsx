97ae79fe9a964d22cd48b7c734f84b59a3c5aae0
BaneleJerry <banelethabede@outlook.com>
2026-04-01 22:20:03 +0200

    minor changes

--- redux-1/app/quotes/page.tsx
+++ redux-1/app/quotes/page.tsx
@@ -4,7 +4,9 @@ export default function QuotesPage() {
   return (
     <>
       <h1>Quotes page</h1>
-      <p>This page is intended to showcase RTK Query.</p>
+      <p>This page is intended to showcase RTK Query. so i can learn
+        
+      </p>
       <Quotes />
     </>
   );
