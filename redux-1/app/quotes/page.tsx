5478990c961dec5c7ca75a7b2ccba648c16bc37c
BaneleJerry <banelethabede@outlook.com>
2026-04-02 23:03:31 +0200

    Gha

--- redux-1/app/quotes/page.tsx
+++ redux-1/app/quotes/page.tsx
@@ -5,7 +5,7 @@ export default function QuotesPage() {
     <>
       <h1>Quotes page</h1>
       <p>This page is intended to showcase RTK Query. so i can learn
-        
+        i am cheating now cause i am using the quotes component that i have already made but i will make a new one using RTK Query and then replace this one with it.
       </p>
       <Quotes />
     </>
